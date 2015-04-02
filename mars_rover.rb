6a02752aa370c9ea3dae9ef3a10af35818223685
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-02 15:00:54 -0400

    created ruby program mar_rover.rb, added the initialize method and broke down the basic steps

---
+++ mars_rover.rb
@@ -0,0 +1,19 @@
+class Mars_Rover
+	def initialize(x, y, direction)
+		@x = x
+		@y = y
+		@direction = direction
+	end
+
+	def move(str)
+
+	end
+
+	def turn_left
+
+	end
+
+	def turn_right
+
+	end
+end
