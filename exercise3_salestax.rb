80b9649c0d5d4d0bfb45a2c477377dbe7db935d2
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-05 20:23:43 -0400

    tried to eliminate the global variables, but changed it back

--- exercise3_salestax.rb
+++ exercise3_salestax.rb
@@ -1,21 +1,25 @@
 class Goods
-	def initialize(quantity, name, basic_price)
+	def initialize(quantity, name, shelf_price)
 		@quantity = quantity
 		@name = name
-		@basic_price = basic_price
-		@basic_sales_tax = 0.10
+		@shelf_price = shelf_price
+		@shelf_sales_tax = 0.10
 	end
 end
 
 class Exempt_Goods < Goods
-	def initialize(quantity, name, basic_price)
-		super(quantity, name, basic_price)
+	def initialize(quantity, name, shelf_price)
+		super(quantity, name, shelf_price)
 	end
 end
 
 class Import_Goods < Goods
-	def initialize(quantity, name, basic_price)
-		super(quantity, name, basic_price)
+	def initialize(quantity, name, shelf_price)
+		super(quantity, name, shelf_price)
 		@import_duty = 0.05
 	end
 end
+
+puts "Enter in items to be purchased:"
+input = get.chomp.upcase
+input_array = input.split
